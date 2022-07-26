602c4c1f2f549e4e67de03f8fb8e470155c513f0
Andrew Auguis <andrew.auguis@gmail.com>
2022-07-26 17:14:38 +0800

    progress

---
+++ src/app/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, Outlet } from 'react-router-dom'
+
+export const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <section>
+                    <h1>Redux Essentials Example</h1>
+                    <div className="navContent">
+                        <div className="navLinks"></div>
+                        <Link to="/posts">Posts</Link>
+                    </div>
+                </section>
+            </nav>
+            <Outlet />
+        </>
+     
+        
+    )
+}
